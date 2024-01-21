b22efdc1874773cf8427ff5515fde3f4b5d08676
P.Sujay Daniel <p.sujaydaniel@PSujays-MacBook-Air.local>
2024-01-21 10:43:04 +0530

    this is ui updated screen

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -30,147 +30,182 @@ class PostScreenState extends State<PostScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 184, 108, 255),
         title: Text("Create Post"),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  controller: taskNameController,
-                  decoration: InputDecoration(labelText: 'Task Name'),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Number of People Required:"),
-                    DropdownButton<int>(
-                      value: selectedNumber,
-                      items: List.generate(
-                        101,
-                        (index) => DropdownMenuItem(
-                          value: index,
-                          child: Text('$index'),
-                        ),
-                      ),
-                      onChanged: (newValue) {
-                        setState(() {
-                          selectedNumber = newValue!;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: DescriptionController,
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                  ),
-                  keyboardType: TextInputType.text,
-                  minLines: 2,
-                  maxLines: 4,
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  controller: locationController,
-                  decoration: InputDecoration(
-                    labelText: 'Site of Work (Address)',
-                  ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  children: [
-                    Text("Type of Work"),
-                    SizedBox(width: 10),
-                    CustomDropdown(
-                      items: ['Contract Worker', 'Wage Per Work'],
-                      onChanged: (selectedValue) {
-                        setState(() {
-                          typeWage = selectedValue == 'Wage Per Work';
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                if (typeWage)
-                  AskInfo(
-                    masonPriceController: masonPriceController,
-                    labourPriceController: LabourPriceController,
-                  ),
-                if (!typeWage)
+      body: Container(
+        color: const Color.fromARGB(255, 250, 225, 255),
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                   TextFormField(
-                    controller: amountController,
-                    decoration: InputDecoration(
-                      labelText: 'Total Amount per Head (in Rs.)',
-                    ),
-                    keyboardType: TextInputType.number,
+                    controller: taskNameController,
+                    decoration: InputDecoration(labelText: 'Task Name'),
                   ),
-                SizedBox(height: 16),
-                Row(
-                  children: [
-                    Text('Will You Pay For Travel'),
-                    Switch(
-                      value: payForTravel,
-                      onChanged: (value) {
-                        setState(() {
-                          payForTravel = value;
-                        });
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Number of People Required:"),
+                      DropdownButton<int>(
+                        value: selectedNumber,
+                        items: List.generate(
+                          101,
+                          (index) => DropdownMenuItem(
+                            value: index,
+                            child: Text('$index'),
+                          ),
+                        ),
+                        onChanged: (newValue) {
+                          setState(() {
+                            selectedNumber = newValue!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: DescriptionController,
+                    decoration: InputDecoration(
+                      labelText: 'Description',
+                    ),
+                    keyboardType: TextInputType.text,
+                    minLines: 2,
+                    maxLines: 4,
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: locationController,
+                    decoration: InputDecoration(
+                      labelText: 'Site of Work (Address)',
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Text("Type of Work"),
+                      SizedBox(width: 10),
+                      CustomDropdown(
+                        items: ['Contract Worker', 'Wage Per Work'],
+                        onChanged: (selectedValue) {
+                          setState(() {
+                            typeWage = selectedValue == 'Wage Per Work';
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  if (typeWage)
+                    AskInfo(
+                      masonPriceController: masonPriceController,
+                      labourPriceController: LabourPriceController,
+                    ),
+                  if (!typeWage)
+                    TextFormField(
+                      controller: amountController,
+                      decoration: InputDecoration(
+                        labelText: 'Total Amount per Head (in Rs.)',
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Text('Will You Pay For Travel'),
+                      Switch(
+                        value: payForTravel,
+                        onChanged: (value) {
+                          setState(() {
+                            payForTravel = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Center(
+                    child: ElevatedButton(
+                      child: Text('Submit'),
+                      onPressed: () async {
+                        log("${AuthService.currentUser?.email!}");
+                        if (_formKey.currentState!.validate()) {
+                          // Create a map containing the data to be stored in Firestore
+                          Map<String, dynamic> postData = {
+                            'taskName': taskNameController.text,
+                            'selectedNumber': selectedNumber,
+                            'description': DescriptionController.text,
+                            'location': locationController.text,
+                            'typeOfWork':
+                                typeWage ? 'Wage Per Work' : 'Contract Worker',
+                            // Add other fields as needed
+                            'uid': AuthService.currentUser!.uid,
+        
+                            'amount': typeWage ? amountController.text : null,
+                            'mason': masonPriceController.text,
+                            'labour': LabourPriceController.text,
+        
+                            'payForTravel': payForTravel,
+                          };
+        
+                          try {
+                            // Add the data to Firestore
+                            // Optionally, you can call your AuthService method here if needed
+                            await AuthService.pushPostScreenData(postData);
+        
+                            // Navigate to the next screen
+                            Navigator.pushNamed(context, '/home');
+                          } catch (e) {
+                            // Handle any errors that occur during the Firestore operation
+                            log('Error adding document to Firestore: $e');
+                          }
+                        }
                       },
                     ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Center(
-                  child: ElevatedButton(
-                    child: Text('Submit'),
-                    onPressed: () async {
-                      log("${AuthService.currentUser?.email!}");
-                      if (_formKey.currentState!.validate()) {
-                        // Create a map containing the data to be stored in Firestore
-                        Map<String, dynamic> postData = {
-                          'taskName': taskNameController.text,
-                          'selectedNumber': selectedNumber,
-                          'description': DescriptionController.text,
-                          'location': locationController.text,
-                          'typeOfWork':
-                              typeWage ? 'Wage Per Work' : 'Contract Worker',
-                          // Add other fields as needed
-                          'uid': AuthService.currentUser!.uid,
-
-                          'amount': typeWage ? amountController.text : null,
-                          'mason': masonPriceController.text,
-                          'labour': LabourPriceController.text,
-
-                          'payForTravel': payForTravel,
-                        };
-
-                        try {
-                          // Add the data to Firestore
-                          // Optionally, you can call your AuthService method here if needed
-                          await AuthService.pushPostScreenData(postData);
-
-                          // Navigate to the next screen
-                          Navigator.pushNamed(context, '/home');
-                        } catch (e) {
-                          // Handle any errors that occur during the Firestore operation
-                          log('Error adding document to Firestore: $e');
-                        }
-                      }
-                    },
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
+
+      ),
+       bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+        ],
+        // currentIndex: currentIndex,
+        // onTap: (int index) {
+        //   setState(() {
+        //     currentIndex = index; // Update the current index
+        //   });
+
+        //   if (currentIndex == 1) {
+        //     Navigator.pushNamed(
+        //         context, '/profile'); // Navigate to the profile screen
+        //   } else if (currentIndex == 2) {
+        //     Navigator.pushNamed(
+        //         context, '/task'); // Navigate to the search screen
+        //   }
+        
       ),
     );
   }
--- lib/screens/sideBar.dart
+++ lib/screens/sideBar.dart
@@ -1,68 +1,170 @@
-import 'package:collarlink/api/api.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:collarlink/api/api.dart'; // Import your API file
 
-class RecentPost extends StatelessWidget {
-  const RecentPost({super.key});
-//adding recent post of current
+class RecentPost extends StatefulWidget {
+  const RecentPost({Key? key}) : super(key: key);
+
+  @override
+  _RecentPostState createState() => _RecentPostState();
+}
+
+class _RecentPostState extends State<RecentPost> {
+  int currentIndex = 0; // Initialize currentIndex
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Recent Post"), centerTitle: true),
-      body: StreamBuilder(
-        stream: AuthService.firestore
-            .collection('users')
-            .doc(AuthService.currentUser?.uid)
-            .collection('recentPost')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                var post = snapshot.data!.docs[index];
-                return ListTile(
-                  title: Text(post['taskName']),
-                  subtitle: Text(post['description']),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () async {
-                      await AuthService.firestore
-                          .collection('users')
-                          .doc(AuthService.currentUser?.uid)
-                          .collection('recentPost')
-                          .doc(post.id)
-                          .delete();
-                    },
-                  ),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text(post['taskName']),
-                          content: Text('Description: ${post['description']}'),
-                          actions: [
-                            TextButton(
-                              child: Text('Close'),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
+      appBar: AppBar(
+      backgroundColor: Color.fromARGB(255, 184, 108, 255),
+      leading: Icon(Icons.menu),
+      actions: [Icon(Icons.search)],
+        title: Text("Recent Post"),
+         centerTitle: true
+         ),
+      body: Container(
+        color: const Color.fromARGB(255, 250, 225, 255),
+        child: StreamBuilder(
+          stream: AuthService.firestore
+              .collection('users')
+              .doc(AuthService.currentUser?.uid)
+              .collection('recentPost')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  var post = snapshot.data!.docs[index];
+                  return ListTile(
+                    title: Text(post['taskName']),
+                    subtitle: Text(post['description']),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () async {
+                        await AuthService.firestore
+                            .collection('users')
+                            .doc(AuthService.currentUser?.uid)
+                            .collection('recentPost')
+                            .doc(post.id)
+                            .delete();
                       },
-                    );
-                  },
-                );
+                    ),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text(post['taskName']),
+                            content: Text('Description: ${post['description']}'),
+                            actions: [
+                              TextButton(
+                                child: Text('Close'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  );
+                },
+              );
+            } else {
+              return Center(child: Text("No data found"));
+            }
+          },
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('John Doe'),
+              accountEmail: Text('john.doe@example.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('https://picsum.photos/200/200'),
+              ),
+            ),
+            ListTile(
+              title: Text('List'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('History'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Recent Post'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/recentPost');
               },
-            );
-          } else {
-            return Center(child: Text("No data found"));
+            ),
+            ListTile(
+              title: Text('Feedback'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Payments'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Logout'),
+              onTap: () {
+                AuthService.signOut();
+                Navigator.pushReplacementNamed(context, '/login');
+              },
+            ),
+            ListTile(
+              title: Row(
+                children: [
+                  Text('Rate Us'),
+                  for (int i = 0; i < 5; i++)
+                    Icon(Icons.star, size: 15, color: Colors.amber),
+                ],
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Message',
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index; // Update the current index
+          });
+
+          if (currentIndex == 0) {
+            Navigator.pushReplacementNamed(
+                context, '/home'); // Navigate to the profile screen
+          } 
+          if (currentIndex == 1) {
+            Navigator.pushNamed(
+                context, '/profile'); // Navigate to the profile screen
+          } 
+          else if (currentIndex == 2) {
+            Navigator.pushNamed(
+                context, '/task'); // Navigate to the search screen
           }
         },
       ),
     );
   }
 }
+
